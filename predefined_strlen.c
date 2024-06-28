251300781be5a0b88f3326baba2fcbf6ffc88ff2
Sidharth K <143076733+sidharth7352@users.noreply.github.com>
2024-06-28 18:56:26 +0530

    string length finding using strlen

---
+++ predefined_strlen.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+ int l;
+ char s[10];
+ printf("enter the string...\n");
+ scanf("%s",s);
+ l=strlen(s);
+ printf("length of string = %d\n",l);
+ }
